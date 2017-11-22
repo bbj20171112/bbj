b830208616708d5cfb62850df2b82c7817f631c3
BinaryLie <1499230032@qq.com>
2017-11-22 16:14:21 +0800

    卡

--- bbj/base/src/main/java/com/bbj/base/BaseDomain.java
+++ bbj/base/src/main/java/com/bbj/base/BaseDomain.java
@@ -15,146 +15,159 @@ import java.util.function.Function;
  * @param <K>
  * @param <V>
  */
-public class BaseDomain<K, V> extends HashMap<K, V>{
+public class BaseDomain<K, V> extends HashMap<K, V>
+{
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    @Override
+    public int size() {
+        return super.size();
+    }
 
-	@Override
-	public int size() {
-		return super.size();
-	}
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return super.isEmpty();
-	}
+    @Override
+    public V get(Object key) {
+        return super.get(key);
+    }
 
-	@Override
-	public V get(Object key) {
-		return super.get(key);
-	}
+    @Override
+    public boolean containsKey(Object key) {
+        return super.containsKey(key);
+    }
 
-	@Override
-	public boolean containsKey(Object key) {
-		return super.containsKey(key);
-	}
+    @Override
+    public V put(K key, V value) {
+        return super.put(key, value);
+    }
 
-	@Override
-	public V put(K key, V value) {
-		return super.put(key, value);
-	}
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        super.putAll(m);
+    }
 
-	@Override
-	public void putAll(Map<? extends K, ? extends V> m) {
-		super.putAll(m);
-	}
+    @Override
+    public V remove(Object key) {
+        return super.remove(key);
+    }
 
-	@Override
-	public V remove(Object key) {
-		return super.remove(key);
-	}
+    @Override
+    public void clear() {
+        super.clear();
+    }
 
-	@Override
-	public void clear() {
-		super.clear();
-	}
+    @Override
+    public boolean containsValue(Object value) {
+        return super.containsValue(value);
+    }
 
-	@Override
-	public boolean containsValue(Object value) {
-		return super.containsValue(value);
-	}
+    @Override
+    public Set<K> keySet() {
+        return super.keySet();
+    }
 
-	@Override
-	public Set<K> keySet() {
-		return super.keySet();
-	}
+    @Override
+    public Collection<V> values() {
+        return super.values();
+    }
 
-	@Override
-	public Collection<V> values() {
-		return super.values();
-	}
+    @Override
+    public Set<java.util.Map.Entry<K, V>> entrySet() {
+        return super.entrySet();
+    }
 
-	@Override
-	public Set<java.util.Map.Entry<K, V>> entrySet() {
-		return super.entrySet();
-	}
+    @Override
+    public V getOrDefault(Object key, V defaultValue) {
+        return super.getOrDefault(key, defaultValue);
+    }
 
-	@Override
-	public V getOrDefault(Object key, V defaultValue) {
-		return super.getOrDefault(key, defaultValue);
-	}
+    @Override
+    public V putIfAbsent(K key, V value) {
+        return super.putIfAbsent(key, value);
+    }
 
-	@Override
-	public V putIfAbsent(K key, V value) {
-		return super.putIfAbsent(key, value);
-	}
+    @Override
+    public boolean remove(Object key, Object value) {
+        return super.remove(key, value);
+    }
 
-	@Override
-	public boolean remove(Object key, Object value) {
-		return super.remove(key, value);
-	}
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        return super.replace(key, oldValue, newValue);
+    }
 
-	@Override
-	public boolean replace(K key, V oldValue, V newValue) {
-		return super.replace(key, oldValue, newValue);
-	}
+    @Override
+    public V replace(K key, V value) {
+        return super.replace(key, value);
+    }
 
-	@Override
-	public V replace(K key, V value) {
-		return super.replace(key, value);
-	}
+    @Override
+    public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+        return super.computeIfAbsent(key, mappingFunction);
+    }
 
-	@Override
-	public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-		return super.computeIfAbsent(key, mappingFunction);
-	}
+    @Override
+    public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        return super.computeIfPresent(key, remappingFunction);
+    }
 
-	@Override
-	public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
-		return super.computeIfPresent(key, remappingFunction);
-	}
+    @Override
+    public V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        return super.compute(key, remappingFunction);
+    }
 
-	@Override
-	public V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
-		return super.compute(key, remappingFunction);
-	}
+    @Override
+    public V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+        return super.merge(key, value, remappingFunction);
+    }
 
-	@Override
-	public V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
-		return super.merge(key, value, remappingFunction);
-	}
+    @Override
+    public void forEach(BiConsumer<? super K, ? super V> action) {
+        super.forEach(action);
+    }
 
-	@Override
-	public void forEach(BiConsumer<? super K, ? super V> action) {
-		super.forEach(action);
-	}
+    @Override
+    public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+        super.replaceAll(function);
+    }
 
-	@Override
-	public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
-		super.replaceAll(function);
-	}
+    @Override
+    public Object clone() {
+        return super.clone();
+    }
 
-	@Override
-	public Object clone() {
-		return super.clone();
-	}
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		return super.equals(o);
-	}
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
-	@Override
-	public String toString() {
-		return super.toString();
-	}
+    public Integer getInt(String key) {
+        return Integer.parseInt(super.get(key).toString());
+    }
+    
+    public Float getFloat(String key) {
+        return Float.parseFloat(super.get(key).toString());
+    }
+    
+    public Double getDouble(String key) {
+        return Double.parseDouble(super.get(key).toString());
+    }
+    
+    public String getStr(String key) {
+        return super.get(key).toString().trim();
+    }
 
 }
