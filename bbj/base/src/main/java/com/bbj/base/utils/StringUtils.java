979607d84f6ee393c94603ac7b2d71d1de1f835c
BinaryLie <1499230032@qq.com>
2017-11-22 16:05:37 +0800

    Signed-off-by: BinaryLie <1499230032@qq.com>

--- bbj/base/src/main/java/com/bbj/base/utils/StringUtils.java
+++ bbj/base/src/main/java/com/bbj/base/utils/StringUtils.java
@@ -1,5 +1,42 @@
 package com.bbj.base.utils;
 
-public class StringUtils {
+public class StringUtils
+{
+
+    /**
+     *  是否为空
+     *  @param obj
+     *  @return    
+     */
+    public static boolean isBlank(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        if ("".equals(obj.toString().trim())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  是否不为空
+     *  @param obj
+     *  @return    
+     */
+    public static boolean isNotBlank(Object obj) {
+        return !isBlank(obj);
+    }
+
+    /**
+     *  是否由数字组成
+     *  @param string
+     *  @return    
+     */
+    public static boolean isDigit(String string) {
+        if (isBlank(string)) {
+            return false;
+        }
+        return string.matches("^[0-9]*$");
+    }
 
 }
