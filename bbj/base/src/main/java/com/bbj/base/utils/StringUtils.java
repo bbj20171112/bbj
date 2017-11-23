f01f79e08895beee999f4eabf0ec36473d4780ed
BinaryLie <1499230032@qq.com>
2017-11-23 09:28:19 +0800

    test

--- bbj/base/src/main/java/com/bbj/base/utils/StringUtils.java
+++ bbj/base/src/main/java/com/bbj/base/utils/StringUtils.java
@@ -4,7 +4,7 @@ public class StringUtils
 {
 
     /**
-     *  是否为空
+     *  对象是否为空
      *  @param obj
      *  @return    
      */
@@ -19,7 +19,7 @@ public class StringUtils
     }
 
     /**
-     *  是否不为空
+     *  对象是否不为空
      *  @param obj
      *  @return    
      */
@@ -37,6 +37,26 @@ public class StringUtils
             return false;
         }
         return string.matches("^[0-9]*$");
+    }
+
+    /**
+     *  生成连续几个相同的字符串
+     *  @param string
+     *          "0"
+     *  @param length
+     *          3
+     *  @return   
+     *          "000" 
+     */
+    public static String getMultipleStrings(String string, int length) {
+        if (isNotBlank(string)) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                sb.append(string);
+            }
+            return sb.toString();
+        }
+        return "";
     }
 
 }
