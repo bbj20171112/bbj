cb8e1c8886e4b9578820871779f05ec9a2e786ca
bage <bage@ASUS-PC>
2017-12-06 22:41:09 +0800

    增加消息返回统一头处理

--- bbj/bbj-base/src/main/java/com/bbj/base/responseconverter/BBJMappingJackson2HttpMessageConverter.java
+++ bbj/bbj-base/src/main/java/com/bbj/base/responseconverter/BBJMappingJackson2HttpMessageConverter.java
@@ -1,78 +1,22 @@
 package com.bbj.base.responseconverter;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonValue;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bbj.base.domain.BBJResponse;
 
-public class BBJMappingJackson2HttpMessageConverter extends AbstractBBJMappingJackson2HttpMessageConverter{
-
-	private String jsonPrefix;
-
-
-	/**
-	 * Construct a new {@link MappingJackson2HttpMessageConverter} using default configuration
-	 * provided by {@link Jackson2ObjectMapperBuilder}.
-	 */
-	public BBJMappingJackson2HttpMessageConverter() {
-		this(Jackson2ObjectMapperBuilder.json().build());
-	}
-
-	/**
-	 * Construct a new {@link MappingJackson2HttpMessageConverter} with a custom {@link ObjectMapper}.
-	 * You can use {@link Jackson2ObjectMapperBuilder} to build it easily.
-	 * @see Jackson2ObjectMapperBuilder#json()
-	 */
-	public BBJMappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
-		super(objectMapper, MediaType.APPLICATION_JSON, new MediaType("application", "*+json"));
-	}
-
-	/**
-	 * Specify a custom prefix to use for this view's JSON output.
-	 * Default is none.
-	 * @see #setPrefixJson
-	 */
-	public void setJsonPrefix(String jsonPrefix) {
-		this.jsonPrefix = jsonPrefix;
-	}
-
-	/**
-	 * Indicate whether the JSON output by this view should be prefixed with ")]}', ". Default is false.
-	 * <p>Prefixing the JSON string in this manner is used to help prevent JSON Hijacking.
-	 * The prefix renders the string syntactically invalid as a script so that it cannot be hijacked.
-	 * This prefix should be stripped before parsing the string as JSON.
-	 * @see #setJsonPrefix
-	 */
-	public void setPrefixJson(boolean prefixJson) {
-		this.jsonPrefix = (prefixJson ? ")]}', " : null);
-	}
-
+public class BBJMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter{
 
 	@Override
-	protected void writePrefix(JsonGenerator generator, Object object) throws IOException {
-		if (this.jsonPrefix != null) {
-			generator.writeRaw(this.jsonPrefix);
-		}
-		String jsonpFunction =
-				(object instanceof MappingJacksonValue ? ((MappingJacksonValue) object).getJsonpFunction() : null);
-		if (jsonpFunction != null) {
-			generator.writeRaw("/**/");
-			generator.writeRaw(jsonpFunction + "(");
-		}
+	protected void writeInternal(Object object, Type type, HttpOutputMessage outputMessage)
+			throws IOException, HttpMessageNotWritableException {
+		System.out.println("object:" + object);
+		super.writeInternal(new BBJResponse("success", object), type, outputMessage);
 	}
 
-	@Override
-	protected void writeSuffix(JsonGenerator generator, Object object) throws IOException {
-		String jsonpFunction =
-				(object instanceof MappingJacksonValue ? ((MappingJacksonValue) object).getJsonpFunction() : null);
-		if (jsonpFunction != null) {
-			generator.writeRaw(");");
-		}
-	}
-
+	
 }
