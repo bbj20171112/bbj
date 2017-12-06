dc784be9e50c515a32d32cbf15bbfe8717e2f58c
bage <bage@ASUS-PC>
2017-12-06 22:41:37 +0800

    增加ajax返回统一头处理

---
+++ bbj/bbj-base/src/main/java/com/bbj/base/responseconverter/BBJRequestResponseBodyMethodProcessor.java
@@ -0,0 +1,29 @@
+package com.bbj.base.responseconverter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
+
+public class BBJRequestResponseBodyMethodProcessor extends RequestResponseBodyMethodProcessor{
+
+	public BBJRequestResponseBodyMethodProcessor(List<HttpMessageConverter<?>> converters) {
+		super(converters);
+	}
+
+	@Override
+	public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest)
+			throws IOException, HttpMediaTypeNotAcceptableException, HttpMessageNotWritableException {
+		System.out.println("returnValue:" + returnValue);
+		super.handleReturnValue(returnValue, returnType, mavContainer, webRequest);
+	}
+
+	
+}
