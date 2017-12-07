bc03024e224c117ecebf0412846aa6fe6f3b6629
bage <bage@192.168.57.2>
2017-12-07 23:06:01 +0800

    增加grid的基本使用

--- bbj/bbj-base/src/main/java/com/bbj/base/controller/WidgetsController.java
+++ bbj/bbj-base/src/main/java/com/bbj/base/controller/WidgetsController.java
@@ -1,9 +1,16 @@
 package com.bbj.base.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 组件控制器
@@ -25,5 +32,28 @@ public class WidgetsController {
 		request.setAttribute("test", "abcd");
 		return "widgets/grid";
 	}
+	/**
+	 * 跳转到grid页面
+	 * @param request
+	 * @return 跳转的页面
+	 */
+	@RequestMapping({"/grid/query"})
+	@ResponseBody
+	public Object grid(@RequestParam(name="tagPage",required=false,defaultValue="1") int tagPage,@RequestParam(name="pageSize",required=false,defaultValue="10") int pageSize){
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		int startId = (tagPage - 1 ) * pageSize;
+		int endId = tagPage * pageSize;
+		for (int i = startId; i < endId; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("id", 1 + i + "");
+			map.put("desc", "这是数据" + (1 + i));
+			list.add(map);
+		}
+		return list;
+	}
+	
+	
+	
+	
 	
 }
