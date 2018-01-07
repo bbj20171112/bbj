cb41b8f290976f728b45a00140d2c99d6801a1ad
bage2014 <893542907@qq.com>
2018-01-07 20:45:40 +0800

    修改codeshow页面的跳转

--- bbj/bbj-base/src/main/java/com/bbj/base/controller/CodeshowController.java
+++ bbj/bbj-base/src/main/java/com/bbj/base/controller/CodeshowController.java
@@ -1,9 +1,8 @@
 package com.bbj.base.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *  组件控制器2
@@ -14,54 +13,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/base/codeshow" })
 public class CodeshowController
 {
-    /**
-     * 跳转到boxdemo页面
+	/**
      * @param request
      * @return 跳转的页面
      */
-    @RequestMapping({"/boxdemo" })
-    public String boxdemo(HttpServletRequest request) {
-        return "codeshow/index?url=boxdemo";
+    @RequestMapping({"/index" })
+    public String boxdemo(@RequestParam(value = "url",defaultValue="boxdemo") String url) {
+        return "widgets/" + url;
     }
-
-    /**
-     * 跳转到cardlistdemo页面
-     * @param request
-     * @return 跳转的页面
-     */
-    @RequestMapping({"/cardlistdemo" })
-    public String cardlistdemo(HttpServletRequest request) {
-        return "codeshow/index?url=cardlistdemo";
-    }
-
-    /**
-     * 跳转到boxdemo页面
-     * @param request
-     * @return 跳转的页面
-     */
-    @RequestMapping({"/echartdemo" })
-    public String echartdemo(HttpServletRequest request) {
-        return "codeshow/index?url=echartdemo";
-    }
-
-    /**
-     * 跳转到richtexteditordemo页面
-     * @param request
-     * @return 跳转的页面
-     */
-    @RequestMapping({"/richtexteditordemo" })
-    public String richtexteditordemo(HttpServletRequest request) {
-        return "codeshow/index?url=richtexteditordemo";
-    }
-
-    /**
-     * 跳转到boxdemo页面
-     * @param request
-     * @return 跳转的页面
-     */
-    @RequestMapping({"/swiperdemo" })
-    public String swiperdemo(HttpServletRequest request) {
-        return "codeshow/index?url=swiperdemo";
-    }
+//    /**
+//     * 跳转到boxdemo页面
+//     * @param request
+//     * @return 跳转的页面
+//     */
+//    @RequestMapping({"/boxdemo" })
+//    public String boxdemo(HttpServletRequest request) {
+//        return "codeshow/index?url=boxdemo";
+//    }
+//
+//    /**
+//     * 跳转到cardlistdemo页面
+//     * @param request
+//     * @return 跳转的页面
+//     */
+//    @RequestMapping({"/cardlistdemo" })
+//    public String cardlistdemo(HttpServletRequest request) {
+//        return "codeshow/index?url=cardlistdemo";
+//    }
+//
+//    /**
+//     * 跳转到boxdemo页面
+//     * @param request
+//     * @return 跳转的页面
+//     */
+//    @RequestMapping({"/echartdemo" })
+//    public String echartdemo(HttpServletRequest request) {
+//        return "codeshow/index?url=echartdemo";
+//    }
+//
+//    /**
+//     * 跳转到richtexteditordemo页面
+//     * @param request
+//     * @return 跳转的页面
+//     */
+//    @RequestMapping({"/richtexteditordemo" })
+//    public String richtexteditordemo(HttpServletRequest request) {
+//        return "codeshow/index?url=richtexteditordemo";
+//    }
+//
+//    /**
+//     * 跳转到boxdemo页面
+//     * @param request
+//     * @return 跳转的页面
+//     */
+//    @RequestMapping({"/swiperdemo" })
+//    public String swiperdemo(HttpServletRequest request) {
+//        return "codeshow/index?url=swiperdemo";
+//    }
 
 }
