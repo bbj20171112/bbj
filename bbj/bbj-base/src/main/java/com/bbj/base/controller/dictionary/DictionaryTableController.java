7f2021b0cacd8617c5ca1c88b07374b8ae3b7157
bage2014 <893542907@qq.com>
2018-01-16 23:00:33 +0800

    提交数据字典程序【未完成】

--- bbj/bbj-base/src/main/java/com/bbj/base/controller/dictionary/DictionaryTableController.java
+++ bbj/bbj-base/src/main/java/com/bbj/base/controller/dictionary/DictionaryTableController.java
@@ -3,13 +3,15 @@ package com.bbj.base.controller.dictionary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bbj.base.domain.SqlFilter;
 import com.bbj.base.domain.dictionary.DictionaryTable;
 import com.bbj.base.service.dictionary.DictionaryTableService;
 
 @Controller
-@RequestMapping(value={"/dictionary/table"})
+@RequestMapping(value={"/base/dictionary/table"})
 public class DictionaryTableController {
 
 	@Autowired
@@ -24,8 +26,30 @@ public class DictionaryTableController {
 	
 
 	@RequestMapping(value={"/deleteById"})
+	@ResponseBody
 	public Object deleteById(String id){
 		return dictionaryTableService.deleteById(id);
+	}
+
+	@RequestMapping(value={"/update"})
+	@ResponseBody
+	public Object update(String id){
+		DictionaryTable bbjEntity = new DictionaryTable();
+		return dictionaryTableService.update(bbjEntity );
+	}
+	
+
+	@RequestMapping(value={"/queryByPage"})
+	@ResponseBody
+	public Object queryByPage(@RequestParam(value="tagPage",defaultValue="1")int tagPage,@RequestParam(value="pageSize",defaultValue="10")int pageSize){
+		SqlFilter<DictionaryTable> sqlFilter = null;
+		return dictionaryTableService.queryByPage(tagPage, pageSize, sqlFilter);
+	}
+	
+
+	@RequestMapping(value={""})
+	public Object index(){
+		return "../framework/dictionary/dictionaryTable";
 	}
 	
 }
