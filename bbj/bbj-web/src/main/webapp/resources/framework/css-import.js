c5f27e2f86429a3a1024b3b59e5dfe06d506da5f
bage2014 <893542907@qq.com>
2018-03-20 23:47:13 +0800

    对于静态资源引入改为： th:href="@{/resources/bower_components/bootstrap/dist/css/bootstrap.min.css}"和  th:src="@{/resources/bower_components/fastclick/lib/fastclick.js}"方式

--- bbj/bbj-web/src/main/webapp/resources/framework/css-import.js
+++ bbj/bbj-web/src/main/webapp/resources/framework/css-import.js
@@ -1,71 +1,37 @@
 /**
  * 引入公共的css样式
  */
-/*var contextPath = 'bbj-web';
-var contextPath = (function() {
-	var path = location.pathname;
-	if (path.indexOf('/') === 0) {
-		path = path.substring(1);
-	}
-	return "/" + path.split('/')[0];
-}());*/
-var contextPath = getRealPath();
 
-function getRealPath() {
-	var localObj = window.location;
-	var contextPath = localObj.pathname.split("/")[1];
-	var basePath = localObj.protocol + "//" + localObj.host + "/" + contextPath;
-	return basePath;
-}
-
-document
-		.writeln('<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/bower_components/bootstrap/dist/css/bootstrap.min.css">');
-document
-		.writeln('<link rel="stylesheet" href="'
-				+ contextPath
-				+ '/resources/bower_components/font-awesome/css/font-awesome.min.css">');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/bower_components/Ionicons/css/ionicons.min.css">');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/dist/css/AdminLTE.css">');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/dist/css/skins/_all-skins.min.css">');
+document.writeln('<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">');
+document.writeln('<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css" th:href="@{/resources/bower_components/bootstrap/dist/css/bootstrap.min.css}">');
+document.writeln('<link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css" th:href="@{/resources/bower_components/font-awesome/css/font-awesome.min.css}">');
+document.writeln('<link rel="stylesheet" href="../bower_components/Ionicons/css/ionicons.min.css" th:href="@{/resources/bower_components/Ionicons/css/ionicons.min.css}">');
+document.writeln('<link rel="stylesheet" href="../dist/css/AdminLTE.css" th:href="@{/resources/dist/css/AdminLTE.css}">');
+document.writeln('<link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css" th:href="@{/resources/dist/css/skins/_all-skins.min.css}">');
 document.writeln('<!--[if lt IE 9]>');
-document
-		.writeln('<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>');
-document
-		.writeln('<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>');
+document.writeln('<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>');
+document.writeln('<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>');
 document.writeln('<![endif]-->');
 
 //document.writeln('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">');
 
 document.writeln('<!-- Pace style -->');
-document
-		.writeln('<link rel="stylesheet" href="'
-				+ contextPath
-				+ '/resources/bower_components/PACE/themes/blue/pace-theme-minimal.css">');
+document.writeln('<link rel="stylesheet" href="../bower_components/PACE/themes/blue/pace-theme-minimal.css" th:href="@{/resources/bower_components/PACE/themes/blue/pace-theme-minimal.css}">');
 
 document.writeln('<!-- ICheck -->');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/plugins/iCheck/all.css">');
+document.writeln('<link rel="stylesheet" href="../plugins/iCheck/all.css" th:href="@{/resources/plugins/iCheck/all.css}">');
 
 document.writeln('<!-- alert -->');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/framework/widgets/css/alert.css">');
+document.writeln('<link rel="stylesheet" href="../framework/widgets/css/alert.css" th:href="@{/resources/framework/widgets/css/alert.css}">');
 
 document.writeln('<!-- grid -->');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/framework/widgets/css/grid.css">');
+document.writeln('<link rel="stylesheet" href="../framework/widgets/css/grid.css" th:href="@{/resources/framework/widgets/css/grid.css}">');
 
 document.writeln('<!-- select2.css -->');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/bower_components/select2/dist/css/select2.css">');
+document.writeln('<link rel="stylesheet" href="../bower_components/select2/dist/css/select2.css" th:href="@{/resources/bower_components/select2/dist/css/select2.css}">');
 
 
 document.writeln('<!-- base.css -->');
-document.writeln('<link rel="stylesheet" href="' + contextPath
-		+ '/resources/framework/common/css/base.css">');
+document.writeln('<link rel="stylesheet" href="../framework/common/css/base.css" th:href="@{/resources/framework/common/css/base.css}">');
 
 
--- bbj/bbj-web/src/main/webapp/resources/framework/javascript-import.js
+++ bbj/bbj-web/src/main/webapp/resources/framework/javascript-import.js
@@ -7,104 +7,75 @@
 document.writeln('<!-- REQUIRED JS SCRIPTS -->');
 
 document.writeln('<!-- jQuery 3 -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/bower_components/jquery/dist/jquery.min.js"></script>');
+document.writeln('<script src="../bower_components/jquery/dist/jquery.min.js" th:src="@{/resources/bower_components/jquery/dist/jquery.min.js}"></script>');
 
 document.writeln('<!-- jQuery UI 1.11.4 -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/bower_components/jquery-ui/jquery-ui.min.js"></script>');
+document.writeln('<script src="../bower_components/jquery-ui/jquery-ui.min.js" th:src="@{/resources/bower_components/jquery-ui/jquery-ui.min.js}"></script>');
 
 document.writeln('<!-- Bootstrap 3.3.7 -->');
 document
-		.writeln('<script src="'
-				+ contextPath
-				+ '/resources/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>');
+		.writeln('<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js" th:src="@{/resources/bower_components/bootstrap/dist/js/bootstrap.min.js}"></script>');
 
 document.writeln('<!-- AdminLTE App -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/dist/js/adminlte.js"></script>');
+document.writeln('<script src="../dist/js/adminlte.js" th:src="@{/resources/dist/js/adminlte.js}"></script>');
 
 document.writeln('<!-- PACE -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/bower_components/PACE/pace.min.js"></script>');
+document.writeln('<script src="../bower_components/PACE/pace.min.js" th:src="@{/resources/bower_components/PACE/pace.min.js}"></script>');
 
 document.writeln('<!-- SlimScroll -->');
 document
-		.writeln('<script src="'
-				+ contextPath
-				+ '/resources/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>');
+		.writeln('<script src="../bower_components/jquery-slimscroll/jquery.slimscroll.min.js" th:src="@{/resources/bower_components/jquery-slimscroll/jquery.slimscroll.min.js}"></script>');
 
 document.writeln('<!-- FastClick -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/bower_components/fastclick/lib/fastclick.js"></script>');
+document.writeln('<script src="../bower_components/fastclick/lib/fastclick.js" th:src="@{/resources/bower_components/fastclick/lib/fastclick.js}"></script>');
 
 document.writeln('<!-- echarts.common.min.js -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/echarts/echarts.common.min.js"></script>');
+document.writeln('<script src="../plugins/echarts/echarts.common.min.js" th:src="@{/resources/plugins/echarts/echarts.common.min.js}"></script>');
 
 document.writeln('<!-- mustache -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/mustache/mustache.js"></script>');
+document.writeln('<script src="../plugins/mustache/mustache.js" th:src="@{/resources/plugins/mustache/mustache.js}"></script>');
 
 document.writeln('<!-- bbj.controls.js -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/bbj.controls.js"></script>');
+document.writeln('<script src="../framework/bbj.controls.js" th:src="@{/resources/framework/bbj.controls.js}"></script>');
 
 document.writeln('<!-- bbj.js -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/bbj.js"></script>');
+document.writeln('<script src="../framework/bbj.js" th:src="@{/resources/framework/bbj.js}"></script>');
 
 document.writeln('<!-- Utils -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/utils/utils.js"></script>');
+document.writeln('<script src="../framework/utils/utils.js" th:src="@{/resources/framework/utils/utils.js}"></script>');
 
 document.writeln('<!-- DataTables -->');
 document.writeln('<!-- grid 基于 DataTables 进行改装 -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/widgets/js/grid.js"></script>');
-// document.writeln('<script
-// src="'+contextPath+'/resources/bower_components/datatables.net/js/jquery.dataTables.js"></script>');
+document.writeln('<script src="../framework/widgets/js/grid.js" th:src="@{/resources/framework/widgets/js/grid.js}"></script>');
+// document.writeln('<script src="../bower_components/datatables.net/js/jquery.dataTables.js"></script>');
 document
-		.writeln('<script src="'
-				+ contextPath
-				+ '/resources/bower_components/datatables.net-bs/js/dataTables.bootstrap.js"></script>');
+		.writeln('<script src="../bower_components/datatables.net-bs/js/dataTables.bootstrap.js" th:src="@{/resources/bower_components/datatables.net-bs/js/dataTables.bootstrap.js}"></script>');
 
 document.writeln('<!-- SlimScroll -->');
 document
-		.writeln('<script src="'
-				+ contextPath
-				+ '/resources/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>');
+		.writeln('<script src="../bower_components/jquery-slimscroll/jquery.slimscroll.min.js" th:src="@{/resources/bower_components/jquery-slimscroll/jquery.slimscroll.min.js}"></script>');
 
 document.writeln('<!-- FastClick -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/bower_components/fastclick/lib/fastclick.js"></script>');
+document.writeln('<script src="../bower_components/fastclick/lib/fastclick.js" th:src="@{/resources/bower_components/fastclick/lib/fastclick.js}"></script>');
 
 document.writeln('<!-- iCheck -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/iCheck/icheck.min.js"></script>');
+document.writeln('<script src="../plugins/iCheck/icheck.min.js" th:src="@{/resources/plugins/iCheck/icheck.min.js}"></script>');
 
 document.writeln('<!-- colResizable -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/colResizable/colResizable-1.5.min.js"></script>');
+document.writeln('<script src="../plugins/colResizable/colResizable-1.5.min.js" th:src="@{/resources/plugins/colResizable/colResizable-1.5.min.js}"></script>');
 
 document.writeln('<!-- SweetAlert -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/sweetalert/sweetalert.min.js"></script>');
+document.writeln('<script src="../plugins/sweetalert/sweetalert.min.js" th:src="@{/resources/plugins/sweetalert/sweetalert.min.js}"></script>');
 
 document.writeln('<!-- layer -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/plugins/layer/layer.min.js"></script>');
+document.writeln('<script src="../plugins/layer/layer.min.js" th:src="@{/resources/plugins/layer/layer.min.js}"></script>');
 
 document.writeln('<!-- alert -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/widgets/js/alert.js"></script>');
+document.writeln('<script src="../framework/widgets/js/alert.js" th:src="@{/resources/framework/widgets/js/alert.js}"></script>');
 
 document.writeln('<!-- select2.js -->');
 document
-		.writeln('<script src="'
-				+ contextPath
-				+ '/resources/bower_components/select2/dist/js/select2.full.js"></script>');
+		.writeln('<script src="../bower_components/select2/dist/js/select2.full.js" th:src="@{/resources/bower_components/select2/dist/js/select2.full.js}"></script>');
 
 document.writeln('<!-- base.js -->');
-document.writeln('<script src="' + contextPath
-		+ '/resources/framework/common/js/base.js"></script>');
+document.writeln('<script src="../framework/common/js/base.js" th:src="@{/resources/framework/common/js/base.js}"></script>');
